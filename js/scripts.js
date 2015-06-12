f5b6f1bd13a80ee6719b3da13d5a362e3aeb9460
Natalia Volodina <Natalia.O.Volodina@gmail.com>
2015-06-12 00:32:07 -0400

    Reworked site stucture. Work in progress

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+$('.about').parallax({imageSrc: "./images/pencil.jpg"});
+//$('#portfolio').parallax({imageSrc: "./images/escapade.jpg"});
+//$('#photography').parallax({imageSrc: "./images/ombres.jpg"});
+//$('#follow').parallax({imageSrc: "./images/abstract.jpg"});
+
+
+          
